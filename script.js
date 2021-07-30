bd62087c9ef87be4c789e7575933e2b03779ba7e
Erickfer1726 <87150725+Erickfer1726@users.noreply.github.com>
2021-07-29 22:26:12 -0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,31 @@
+document.getElementById("resultados").style.display = "";
+function ConvertToRoman() {
+  let num = palindrome = document.getElementById("inputConversor").value;
+  let decimalValue = [1000, 900, 500, 400, 100, 90, 50, 40, 10, 9, 5, 4, 1];
+  let romanNumeral = [
+    "M",
+    "CM",
+    "D",
+    "CD",
+    "C",
+    "XC",
+    "L",
+    "XL",
+    "X",
+    "IX",
+    "V",
+    "IV",
+    "I"
+  ];
+
+  let romanized = "";
+
+  for (let index = 0; index < decimalValue.length; index++) {
+    while (decimalValue[index] <= num) {
+      romanized += romanNumeral[index];
+      num -= decimalValue[index];
+    }
+  }
+
+  return document.getElementById("resultados").value = romanized;
+}
